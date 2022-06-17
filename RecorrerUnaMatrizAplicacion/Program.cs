76020dd2fc98e0fb05638017050f07458ec9e99f
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-17 11:58:58 -0500

    Update pruebas unitarias

--- RecorrerUnaMatrizAplicacion/Program.cs
+++ RecorrerUnaMatrizAplicacion/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RecorrerUnaMatrizBiblioteca;
+
+Console.WriteLine("Recorriendo una Matriz!");
+
+Algoritmo algoritmoRecorrerMatriz = new Algoritmo();
+int k = 3;
+string matrizRecorrida = algoritmoRecorrerMatriz.RecorrerMatriz(k);
+Console.WriteLine("k={0}, MatrizRecorrida={1}", k, matrizRecorrida);
--- RecorrerUnaMatrizBiblioteca/Algoritmo.cs
+++ RecorrerUnaMatrizBiblioteca/Algoritmo.cs
@@ -12,11 +12,12 @@
             matriz.Add("7");
             matriz.Add("6");
             //Recorremos el numero de veces K la matriz
-            for (int i = 0; i == k; i++)
+            for (int i = 0; i < k; i++)
             {
                 string numeroaRecorrer = matriz[matriz.Count - 1];
                 matriz.Remove(numeroaRecorrer);
-                matriz.Add(numeroaRecorrer);
+                matriz.Insert(0,numeroaRecorrer);
+                
             }
             //imprimir cada uno de los valores de la lista
             string cadena = matriz.Aggregate((x, y) => x + "," + y);
